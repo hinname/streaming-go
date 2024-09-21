556c3af4558ffd8f28b1e187337a1440af025dc4
Allan Resende <allanresendegomes@outlook.com>
2024-09-21 11:59:52 -0300

    Vue Reactivity playaround

--- front/app.vue
+++ front/app.vue
@@ -1,6 +1,31 @@
+<script setup lang="ts">
+const url = window?.document?.URL;
+interface A {
+  a: string,
+  b: string,
+}
+const a: A = {
+  a: 'a',
+  b: url,
+};
+const logFunction = (object: A) => {
+  console.log(`URL: ${object.b}`);
+}
+logFunction(a);
+const hello = ref('Hello World');
+const inputVal = ref('');
+
+watchEffect(() => {
+  hello.value = inputVal.value ? inputVal.value : 'Hello World';
+  console.log(inputVal.value);
+});
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1>
+    {{hello}}
+  </h1>
+  <hr>
+  <input v-model="inputVal"/>
 </template>
